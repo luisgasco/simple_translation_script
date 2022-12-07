b368713aa33ff28e65deca440452160666442023
lgascosa <luisgascosanchez@gmail.com>
2022-12-07 12:48:09 +0100

    Include tqdm progress bar

--- simple_translation_script.py
+++ simple_translation_script.py
@@ -15,15 +15,6 @@ from optparse import OptionParser
 import torch
 from torch.utils.data import Dataset
 from tqdm.auto import tqdm
-class ListDataset(Dataset):
-     def __init__(self, original_list):
-        self.original_list = original_list
-     def __len__(self):
-        return len(self.original_list)
-
-     def __getitem__(self, i):
-        return self.original_list[i]
-
 
 def main(argv=None):
     parser = OptionParser()
@@ -38,11 +29,12 @@ def main(argv=None):
     parser.add_option("--output_column", dest="output_column", default = "translated_column", help = "Named of the new columns in which you want to save the translation (only if file_type=='tsv')")
     (options, args) = parser.parse_args(argv)
 
-    # Leer archivo. De momento archivo con un documento por línea. Más adelante más opciones
+    # Leer archivo. De momento archivo con un documento por línea. 
     if options.file_type=="lines":
         # Read file
         with open(options.input_path) as file:
             lines_to_translate = [line.strip() for line in file]
+    # Leer archivo tsv
     elif options.file_type=="tsv":
         # Read file
         df_translate = pd.read_csv(options.input_path, sep="\t")
@@ -61,24 +53,21 @@ def main(argv=None):
                             device= torch.device(0 if torch.cuda.is_available() else 'cpu') ) 
  
     # Traducir
-    inner_sub_list = 100
+    inner_sub_list = 100 # This value can be modify if needed
     # Finalmente, subdividimos la lista en listas menores para aplicar tqdm
     sublistas = [lines_to_translate[x:x+inner_sub_list] for x in range(0, len(lines_to_translate), inner_sub_list)]
     
-    # Puede arrojar un warning por no ser eficiente con GPU, pero es mucho más rápido así que sin hacer
-    # las sublistas
+    # Puede arrojar un warning por no ser eficiente con GPU, pero es mucho más rápido así que sin hacer las sublistas
     lista_output = list()
     for sublista in tqdm(sublistas, desc="Procesando sublistas de {} elementos".format(inner_sub_list)):
         lista_output.extend(pipeline_mt(sublista, batch_size=4))
 
-    # Old way to translate list without tqdm
-    # BAD PERFORMANCE with very big lists.
-    #results = pipeline_mt(lines_to_translate, 
-    #                    batch_size=4)
+    # NOTE:  Old way to translate list without tqdm- BAD PERFORMANCE with very big lists.
+    #results = pipeline_mt(lines_to_translate, batch_size=4)
 
-    # Generamos lista final de salida
-    # Next line is faster than translated_input = [t["translation_text"] for t in results] 
+    # Generamos lista final de salida] 
     translated_input = list(map(lambda x: x["translation_text"], lista_output))
+
     # Guardar archivo en el mismo formato que la entrada (dependerá de file_type)
     if options.file_type == "lines":
         with open(options.output_path, 'w') as output:
